62c5ee95fffd965680cb3167975012c79fd4e4fa
Kazu Fukushige <kimjoshjp@gmail.com>
2023-01-20 18:57:24 +1000

    Update main.tf
    updated

--- main.tf
+++ main.tf
@@ -2,26 +2,30 @@
 #
 # deploy vm in google cloud env
 
+variable "GOOGLE_CREDENTIALS" {}
+variable "PROJECT_ID" {}
+
 provider "google" {
-  project = "{{TEST001}}"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  credentials = "${var.GOOGLE_CREDENTIALS}"
+  project     = "${var.PROJECT_ID}"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+resource "google_compute_instance" "tf-cloud-01" {
+    name         = "tf-cloud-01"
+    machine_type = "f1-micro"
+    allow_stopping_for_update = true
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+    boot_disk {
+        initialize_params {
+        size  = 10
+        type  = "pd-standard"
+        image = "debian-cloud/debian-9"
+        }
     }
-  }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
+	network_interface {
+		network = "default"
+	}
 }
